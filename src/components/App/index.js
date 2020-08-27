3545fabbc4af489334b8ec5e475c152493f1542a
Loïc ZHU <zhu.loic@gmail.com>
2020-08-27 10:28:50 +0200

    replace static conversion informations + proptypes in Conversion

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -11,12 +11,25 @@ import currenciesList from 'src/data/currencies';
 // == Composant
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
+  state = {
+    conversion: {
+      conversionAmount: currenciesList[1].rate,
+      conversionCurrencyName: currenciesList[1].name,
+    },
+  };
+
   render() {
+    // eslint-disable-next-line react/destructuring-assignment
+    const { conversionAmount, conversionCurrencyName } = this.state.conversion;
+
     return (
       <div className="app">
         <Converter />
         <Currencies currenciesList={currenciesList} />
-        <Conversion />
+        <Conversion
+          conversionAmount={conversionAmount}
+          conversionCurrencyName={conversionCurrencyName}
+        />
       </div>
     );
   }
--- src/components/Conversion/index.js
+++ src/components/Conversion/index.js
@@ -1,16 +1,23 @@
 // import npm
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // import
 import './conversion.scss';
 
 // component
-const Conversion = () => (
+const Conversion = ({ conversionAmount, conversionCurrencyName }) => (
   <div className="conversion">
-    <div className="conversion__value">1.09</div>
-    <div className="conversion__currency">USD</div>
+    <div className="conversion__value">{conversionAmount}</div>
+    <div className="conversion__currency">{conversionCurrencyName}</div>
   </div>
 );
+
+// props validation
+Conversion.propTypes = {
+  conversionAmount: PropTypes.number.isRequired,
+  conversionCurrencyName: PropTypes.string.isRequired,
+};
 
 // export
 export default Conversion;
